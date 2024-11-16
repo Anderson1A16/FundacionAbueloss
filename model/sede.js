9a2878943f19e16d92de3b024d5d3498c7bca115
Anderson1A16 <quinonesanderson615@gmail.com>
2024-11-16 11:05:58 -0500

    sede lista

--- model/sede.js
+++ model/sede.js
@@ -10,16 +10,16 @@ const Sedes = {
             return rows [0];
     },
     create:async(Sede_id,Sede_nombre,Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos) => {
-        const [result] = await db.query('Insert into sql10744906.sede(Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos) values (?,?,?,?,?,?,?)',
+        const [result] = await db.query('Insert into sql10744906.Sede(Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos) values (?,?,?,?,?,?,?)',
             [Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos]);
         return {Sede_id:result.affectedRows.Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos};
     },
     update:async(Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos) =>{
-        const [result] = await db.query('Update sql10744906.sede set Sede_nombre=?, Sede_direccion=?, Sede_ciudad=?, Sede_barrio=?, Sede_capacidad=?, Sede_cupos=? where Sede_id=?', [Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos, Sede_id]);
+        const [result] = await db.query('Update sql10744906.Sede set Sede_nombre=?, Sede_direccion=?, Sede_ciudad=?, Sede_barrio=?, Sede_capacidad=?, Sede_cupos=? where Sede_id=?', [Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos, Sede_id]);
         return {Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos};
     },
     delete:async(Sede_id)=>{
-        await db.query('delete from sql10744906.sede where Sede_id=?', [Sede_id]);
+        await db.query('delete from sql10744906.Sede where Sede_id=?', [Sede_id]);
         return {Sede_id};
     }
 
