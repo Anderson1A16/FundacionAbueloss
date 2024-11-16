54d36dff75f8eff68763d20d99ca740e08e67696
Anderson1A16 <quinonesanderson615@gmail.com>
2024-11-16 10:45:54 -0500

    Corregir sede

--- model/sede.js
+++ model/sede.js
@@ -2,7 +2,7 @@ const db = require('../config/mysql');
 
 const Sedes = {
     getAll: async() =>{
-        const[rows] = await db.query('select Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos from sede');
+        const[rows] = await db.query('select Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos from sql10744906.sede');
         return rows;
     },
     getById:async(Sede_id) =>{
