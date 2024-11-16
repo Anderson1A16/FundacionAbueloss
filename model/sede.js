bdc33230e8d27d2818440f2defebdf24cc7420c0
Anderson1A16 <quinonesanderson615@gmail.com>
2024-11-16 10:57:16 -0500

    corregir

--- model/sede.js
+++ model/sede.js
@@ -2,11 +2,11 @@ const db = require('../config/mysql');
 
 const Sedes = {
     getAll: async() =>{
-        const[rows] = await db.query('select Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos from sql10744906.sede');
+        const[rows] = await db.query('select Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos from sql10744906.Sede');
         return rows;
     },
     getById:async(Sede_id) =>{
-        const [rows] = await db.query('select Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos from sql10744906.sede where Sede_id=?', [Sede_id]);
+        const [rows] = await db.query('select Sede_id, Sede_nombre, Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos from sql10744906.Sede where Sede_id=?', [Sede_id]);
             return rows [0];
     },
     create:async(Sede_id,Sede_nombre,Sede_direccion, Sede_ciudad, Sede_barrio, Sede_capacidad, Sede_cupos) => {
